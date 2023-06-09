e8d8527844ddb383646038e5fb4b8f4281e2f2bd
Lavínia Rodrigues <lavinia.rodriguesr@gmail.com>
2023-06-09 13:19:11 -0300

    conexão cliente servidor atualizada

--- cliente.py
+++ cliente.py
@@ -1,12 +1,28 @@
 import socket
-HOST = ' '     # Endereco IP do Servidor
-PORT = 5000            # Porta que o Servidor esta
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-dest = (HOST, PORT)
-tcp.connect(dest)
-print 'Para sair use CTRL+X\n'
-msg = raw_input()
-while msg <> '\x18':
-    tcp.send (msg)
-    msg = raw_input()
-tcp.close()
+
+def connect_to_server():
+    # Configurando o host e a porta do servidor
+    host = '192.168.0.11'  # Use o IP correto do servidor
+    port = 5000
+
+    # Criando um socket TCP/IP
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Conectando ao servidor
+    client_socket.connect((host, port))
+    print(f"Conectado ao servidor {host}:{port}")
+
+    # Lógica do cliente
+    # Aqui você pode implementar a interação com o usuário para escolher um número da rifa
+
+    # Enviando a escolha do cliente para o servidor
+    client_socket.send("42".encode())
+
+    # Recebendo a resposta do servidor
+    response = client_socket.recv(1024).decode()
+    print("Resposta do servidor:", response)
+
+    # Fechando a conexão com o servidor
+    client_socket.close()
+
+connect_to_server()
--- servidor.py
+++ servidor.py
@@ -1,16 +1,39 @@
 import socket
-HOST = ''              # Endereco IP do Servidor
-PORT = 5000            # Porta que o Servidor esta
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-orig = (HOST, PORT)
-tcp.bind(orig)
-tcp.listen(1)
-while True:
-    con, cliente = tcp.accept()
-    print 'Concetado por', cliente
+import threading
+
+def handle_client(client_socket):
+    # Lógica para tratar as requisições do cliente
+    # Aqui você pode implementar a lógica do sorteio da rifa
+
+    # Exemplo de envio de uma mensagem de resposta para o cliente
+    response = "O número sorteado é: 42"
+    client_socket.send(response.encode())
+
+    # Fechando a conexão com o cliente
+    client_socket.close()
+
+def start_server():
+    # Configurando o host e a porta do servidor
+    host = '192.168.0.11'  # Use o IP correto do servidor
+    port = 5000
+
+    # Criando um socket TCP/IP
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Ligando o socket ao host e à porta
+    server_socket.bind((host, port))
+
+    # Habilitando o servidor para aceitar conexões
+    server_socket.listen(5)
+    print(f"Servidor ouvindo em {host}:{port}")
+
     while True:
-        msg = con.recv(1024)
-        if not msg: break
-        print cliente, msg
-    print 'Finalizando conexao do cliente', cliente
-    con.close()
+        # Aceitando a conexão de um cliente
+        client_socket, address = server_socket.accept()
+        print(f"Conexão estabelecida com {address[0]}:{address[1]}")
+
+        # Criando uma nova thread para tratar o cliente
+        client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+        client_thread.start()
+
+start_server()
